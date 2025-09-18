f17ad5cb1b38c21445d5db4d946517ab2bff629e
Mallikarjuna <spiderman05116@gmail.com>
2025-09-18 17:36:40 +0530

    Update AddUser.js

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -19,7 +19,8 @@ export default function AddUser() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:8080/user", user);
+    //await axios.post("http://localhost:8080/user", user);
+    await axios.post("https://userhub-backend.onrender.com", user);
     navigate("/");
   };
 
